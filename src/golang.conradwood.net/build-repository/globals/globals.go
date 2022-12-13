8777b0f438feca36f2938fc43eb65aefd579113c
Conrad Wood <cnw@conradwood.net>
2022-12-13 14:40:48 +0000

    .

---
+++ src/golang.conradwood.net/build-repository/globals/globals.go
@@ -0,0 +1,16 @@
+package globals
+
+import (
+	"time"
+)
+
+var (
+	last_upload time.Time
+)
+
+func UploadCompleted() {
+	last_upload = time.Now()
+}
+func LastUploadCompleted() time.Time {
+	return last_upload
+}
